406ef00e61c285f3dea12a1b90ed461e9826f1ed
takyi-ishmael <takyiishmael5@gmail.com>
2023-02-16 17:16:16 -0800

    this is a commit file

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,9 +1,10 @@
 #include <stdio.h>
 /**
-* main - A program that prints a line with puts function
+* main - entry
 * Return: 0 
 */
 int main(void)
 {
-puts("\"Programming is like building a multilingual puzzle");
-return (0);
+	puts("\"Programming is like building a multilingual puzzle");
+	return (0);
+}
